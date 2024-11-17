145997e6e3d4ee3d02e7ae017dd63c182decf84a
Kostiantyn <114479855+tarantino421@users.noreply.github.com>
2024-11-17 14:25:04 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
 var swiper = new Swiper(".heroSwiper", {
   autoplay: {
-    delay: 3000, // Змінено на 3000 мс (3 секунди)
+    delay: 4000, // Затримка між слайдами (3 секунди)
+    disableOnInteraction: false, // Автопрокрутка не припиняється при взаємодії з слайдером
   },
-  speed: 1000,
-  effect: "fade",
+  loop: true, // Безкінечне прокручування (слайдер буде повертатися до першого слайду після останнього)
+  speed: 2000, // Швидкість переходу між слайдами (1 секунда)
+  effect: "fade", // Ефект переходу (згасання)
+  allowTouchMove: false, // Забороняє взаємодію через мишу або торкання
+});
+
+document.querySelector(".to-book-btn").addEventListener("click", function () {
+  // Прокрутка до форми
+  const formElement = document.getElementById("form");
+  formElement.scrollIntoView({
+    behavior: "smooth",
+    block: "start",
+  });
 });
