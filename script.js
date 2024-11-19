d9be19240fe5ec3fbd1d614dfa4504af55f238ce
Kostiantyn <114479855+tarantino421@users.noreply.github.com>
2024-11-20 01:42:35 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var swiper = new Swiper(".heroSwiper", {
   autoplay: {
-    delay: 4000, // Затримка між слайдами (3 секунди)
+    delay: 5000, // Затримка між слайдами (3 секунди)
     disableOnInteraction: false, // Автопрокрутка не припиняється при взаємодії з слайдером
   },
   loop: true, // Безкінечне прокручування (слайдер буде повертатися до першого слайду після останнього)
@@ -17,3 +17,27 @@ document.querySelector(".to-book-btn").addEventListener("click", function () {
     block: "start",
   });
 });
+
+/////
+
+const track = document.querySelector(".logos__track");
+
+let clone1 = track.innerHTML;
+track.innerHTML += clone1;
+
+let start = 0;
+const speed = 0.25;
+const trackWidth = track.offsetWidth / 1;
+
+function scroll() {
+  start -= speed;
+  if (start <= -trackWidth) {
+    start = 0;
+  }
+  track.style.transform = `translateX(${start}px)`;
+  track.style.transition = "transform 0s linear";
+
+  requestAnimationFrame(scroll);
+}
+
+scroll();
